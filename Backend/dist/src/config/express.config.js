9f307f0b5cfe2deae0a43ad3f5302bbfcfbfd106
Hadi <969hadiahmed@gmail.com>
2025-06-24 20:30:54 +0530

    wip

--- Backend/dist/src/config/express.config.js
+++ Backend/dist/src/config/express.config.js
@@ -5,11 +5,12 @@ import { getStoreDetails, getStores } from "../controllers/store.controller";
 import cors from "cors";
 import { addView } from "../controllers/view.controller";
 const port = process.env.PORT || 3000;
+const origin = process.env.FRONTEND_URL;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
 }));
 //---------routes------------------
 app.get('/', asyncHadnler(async (_req, res) => {
--- Backend/src/config/express.config.ts
+++ Backend/src/config/express.config.ts
@@ -6,12 +6,12 @@ import cors from "cors"
 import { addView } from "../controllers/view.controller";
 import { Request, Response } from "express";
 const port = process.env.PORT || 3000;
-
+const origin= process.env.FRONTEND_URL;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin:'*',
 }));
 //---------routes------------------
 app.get('/', asyncHadnler(
